941e9e6efbc7ab21ec4accf8a31f92a07e1b9e76
uniquemozilla22 <uniqgaming26@gmail.com>
2021-06-14 21:22:38 +0545

    Appointment Screeen and Category Screen Created Admin

---
+++ src/admin/Appointment/ListItem.js
@@ -0,0 +1,62 @@
+import React, { useRef } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Card, Avatar } from "react-native-paper";
+import colors from "../../assets/colors/colors";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+const ListItem = ({ appointment, onOpen, index }) => {
+  const { name, image } = appointment.feild;
+  const username = appointment.user.name;
+  const { date } = appointment;
+
+  return (
+    <>
+      <TouchableOpacity onPress={() => onOpen(index)}>
+        <Card style={styles.container}>
+          <Card.Title
+            title={<Text style={styles.title}>{username}</Text>}
+            subtitle={
+              <Text style={styles.subtitle}>
+                {new Date(date).toDateString()}
+              </Text>
+            }
+            left={(props) => (
+              <Avatar.Image {...props} size={40} source={{ uri: image }} />
+            )}
+            right={(props) => (
+              <AntDesign
+                name={"delete"}
+                color={colors.red}
+                size={20}
+              ></AntDesign>
+            )}
+          />
+        </Card>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    padding: 5,
+    paddingHorizontal: 20,
+    marginVertical: 5,
+    borderColor: colors.greengrey,
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderRadius: 20,
+  },
+  title: {
+    color: colors.black,
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  subtitle: {
+    color: colors.grey,
+    fontSize: 12,
+  },
+});
+
+export default ListItem;
